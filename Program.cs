4d17ca37d6fb275c1b06abbbc88ba38c7baf5c6a
Nathan Elmer <nelmer12@gmail.com>
2022-03-01 15:41:06 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+int RandomNumber()
+{
+    int dieValue = new Random().Next(1, 50);
+    return dieValue;
+}
+
+int secretNumber = RandomNumber();
+
+Console.WriteLine("Guess and try to figure out my secret number in 4 attempts: ");
+
+var input = int.Parse(Console.ReadLine());
+
+if (input == secretNumber)
+{
+    Console.WriteLine("You hacker.");
+}
+else
+{
+    for (int i = 2; i < 5; i++)
+    {
+        if (input < secretNumber)
+        {
+            Console.WriteLine("Too Low!");
+        }
+        else
+        {
+            Console.WriteLine("Too High!");
+        }
+        Console.Write($"(Attempt {i}) Try again: ");
+        input = int.Parse(Console.ReadLine());
+        if (input == secretNumber)
+        {
+            Console.WriteLine("Good job! You guessed it.");
+            break;
+        }
+    }
+    Console.WriteLine($"The number was {secretNumber}");
+}
+
+
+
+
